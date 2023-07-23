64492bf7e95f616ef30bfe4b580ff53e7cae2942
alinegmayworm <alinegmayworm@gmail.com>
2023-07-23 03:39:20 +0100

    fix(WS-1): Added missing import

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
-
 import { WelcomeComponent } from "./welcome/welcome.component";
 
 @NgModule({
